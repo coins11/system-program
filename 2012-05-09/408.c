d0dcedfb1cb7bf5b34f21347694d961e0d21d7b5
Kazuhiko Sakaguchi <pi8027@gmail.com>
2012-05-14 22:30:27 +0900

    2012-05-09

--- 2012-05-09/408.c
+++ 2012-05-09/408.c
@@ -4,7 +4,12 @@ pid_t fork_interactive(int *pipe_fd)
 {
     int pipe_fd1[2], pipe_fd2[2];
     pid_t pid;
-    if(pipe(pipe_fd1) < 0 || pipe(pipe_fd2) < 0) return -1;
+    if(pipe(pipe_fd1) < 0) return -1;
+    if(pipe(pipe_fd2) < 0){
+        close(pipe_fd1[0]);
+        close(pipe_fd1[1]);
+        return -1;
+    }
     pid = fork();
     close(pipe_fd1[!pid]);
     close(pipe_fd2[!!pid]);
@@ -21,7 +26,7 @@ ssize_t writeb(int fd, char c)
 
 int main(int argc, char** argv)
 {
-    int pnum, onum, pcount = 0, ocount = 0, pipe_fd[4];
+    int pnum, onum, pcount = 0, ocount = 0, pipe_fd[4], status;
     pid_t pid;
     char buf[1];
     if(argc < 3) return -1;
@@ -43,17 +48,26 @@ int main(int argc, char** argv)
             }
             else if(writeb(1, '0'+(ocount>>1)%10) < 0) return -1;
         }
+        close(pipe_fd[2]);
+        close(pipe_fd[3]);
         if(writeb(1, '\n') < 0) return -1;
     }
     else{
         for(; read(pipe_fd[0], buf, 1) > 0; ocount++){
             if(ocount%(pnum+1-pcount)){
-                if(write(pipe_fd[2], buf, 1) < 0) return -1;
+                if(writeb(pipe_fd[2], buf[0]) < 0) return -1;
                 read(pipe_fd[3], buf, 1);
             }
-            else if(write(1, buf, 1) < 0) return -1;
-            if(write(pipe_fd[1], buf, 1) < 0) return -1;
+            else if(writeb(1, buf[0]) < 0) return -1;
+            if(writeb(pipe_fd[1], buf[0]) < 0) return -1;
+        }
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+        if(pcount != pnum){
+            close(pipe_fd[2]);
+            close(pipe_fd[3]);
         }
     }
+    waitpid(pid, &status, 0);
     return 0;
 }
